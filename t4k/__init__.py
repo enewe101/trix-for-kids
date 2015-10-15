5776272a73b350e17f3ab50bfa82b4a0069bbeb1
Edward Newell <edward.newell@gmail.com>
2015-10-15 12:27:51 -0400

    add selenium_crawler and managed_process

--- t4k/__init__.py
+++ t4k/__init__.py
@@ -6,9 +6,7 @@ from persistent_ordered_dict import (
 from safe import (
 	safe_min, safe_max, safe_lte, safe_lt, safe_gte, safe_gt
 )
-from tsv import (
-	UnicodeTsvReader, UnicodeTsvWriter
-)
-from file_utils import (
-	lsfiles
-)
+from tsv import UnicodeTsvReader, UnicodeTsvWriter
+from file_utils import lsfiles
+from managed_process import ManagedProcess
+from selenium_crawler import SeleniumCrawler, uses_selenium
