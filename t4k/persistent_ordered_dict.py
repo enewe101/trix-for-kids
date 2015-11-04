fc0e4b9fa0c3535fe3f308888a20b9e13125dc35
Edward Newell <edward.newell@gmail.com>
2015-11-04 17:38:19 -0500

    try to fix pod after lsfiles -> ls

--- t4k/persistent_ordered_dict.py
+++ t4k/persistent_ordered_dict.py
@@ -94,7 +94,7 @@ class PersistentOrderedDict(object):
 
 			# ensure that files are in expected order,
 			# that none are missing, and that no lines are missing.
-			assert(fname == '%d.json' % i)
+			assert(fname == os.path.join(self.path, '%d.json' % i))
 			if i > 0:
 				prev_file_path = os.path.join(self.path, '%d.json' % (i-1))
 				num_lines_prev_file = len(
@@ -109,7 +109,7 @@ class PersistentOrderedDict(object):
 
 			i += 1
 
-			for entry in open(os.path.join(self.path, fname)):
+			for entry in open(os.path.join(fname)):
 
 				# skip blank lines (there's always one at end of file)
 				if entry=='':
