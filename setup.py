cb38f51d5913a2c4288da87a40f3ab2bbd5d073e
Edward Newell <edward.newell@gmail.com>
2017-01-28 19:53:23 -0500

    update setup.py, rename README

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from setuptools import setup
 
 setup(
 	name='t4k',
-	version='0.4.0',
+	url='http://drgitlab.cs.mcgill.ca/enewe101/trix_for_kids',
+	version='0.5.0',
 	description='my personal swiss army knife',
 	author='edward newell',
 	author_email='edward.newell@gmail.com',
